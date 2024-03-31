e424f60cca3c2afe84e482ecafeef10a79303473
Suraj Gund <surajgund46@gmail.com>
2024-03-31 18:35:36 +0530

    First Step Complete

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mulish","sans-serif"],
+      },
+      colors:{
+        deepBlue:"#020421",
+        lightBlue300:"#4b94ed",
+        lighBlue500:"#0b72e7",
+        greenLight:"#61cea6",
+        grayText:"#818597",
+        lightGray:"#e2e2e2",
+        grayBlue:"#344a6c",
+        deepBlueHead:"#a62fs6",
+        gray2:"525a76",
+
+      },
+    },
+  },
+  plugins: [],
+}
+
